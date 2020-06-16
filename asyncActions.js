f92b3917e41dedb7cea1f040cbc9548f203ff8cd
Ali Eldamiry <alieldamiry@gmail.com>
2020-06-16 20:46:19 +0200

    Redux Thunk Middleware

--- asyncActions.js
+++ asyncActions.js
@@ -1,5 +1,8 @@
 const redux = require('redux');
 const createStore = redux.createStore;
+const applyMiddleware = redux.applyMiddleware;
+const thunkMiddleware = require('redux-thunk').default;
+const axios = require('axios');
 
 const initialState = {
     loading: false,
@@ -7,9 +10,11 @@ const initialState = {
     error: ''
 }
 
-const FETCH_USERS_REQUEST = 'FETCH_USERS_REQUEST';
-const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS';
-const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE';
+const FETCH_USERS_REQUEST = 'FETCH_USERS_REQUEST'
+const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS'
+const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE'
+
+
 
 const fetchUsersRequest = () => {
     return {
@@ -17,7 +22,7 @@ const fetchUsersRequest = () => {
     }
 }
 
-const fetchUsersSuccesst = users => {
+const fetchUsersSuccess = users => {
     return {
         type: FETCH_USERS_SUCCESS,
         payload: users
@@ -28,6 +33,23 @@ const fetchUsersFailure = error => {
     return {
         type: FETCH_USERS_FAILURE,
         payload: error
+    }
+}
+
+const fetchUsers = () => {
+    return function (dispatch) {
+        dispatch(fetchUsersRequest())
+        axios
+            .get('https://jsonplaceholder.tycode.com/users')
+            .then(response => {
+                // response.data is the users
+                const users = response.data.map(user => user.id)
+                dispatch(fetchUsersSuccess(users))
+            })
+            .catch(error => {
+                // error.message is the error message
+                dispatch(fetchUsersFailure(error.message))
+            })
     }
 }
 
@@ -55,4 +77,6 @@ const reducer = (state = initialState, action) => {
     }
 }
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+store.subscribe(() => { console.log(store.getState()) });
+store.dispatch(fetchUsers());
